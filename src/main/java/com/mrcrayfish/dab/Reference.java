3e1b669229221a33a183a8327c6b67d9faa2533f
MrCrayfish <mrcrayfish@hotmail.com>
2017-01-30 21:38:37 +1030

    Updated version info

--- src/main/java/com/mrcrayfish/dab/Reference.java
+++ src/main/java/com/mrcrayfish/dab/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "cdabm";
 	public static final String MOD_NAME = "MrCrayfish's Dab Mod";
-	public static final String MOD_VERSION = "1.0.2";
+	public static final String MOD_VERSION = "1.0.3";
 	public static final String MC_VERSION = "[1.10,1.10.2]";
 	public static final String DEPENDENCIES = "required-after:RenderPlayerAPI@[1.1,)";
 	public static final String PROXY_CLIENT = "com.mrcrayfish.dab.proxy.ClientProxy";
