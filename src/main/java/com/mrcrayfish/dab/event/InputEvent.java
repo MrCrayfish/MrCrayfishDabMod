9a4fa9183d21f450e1992f8680cf909551e04c2f
MrCrayfish <mrcrayfish@hotmail.com>
2017-04-09 12:23:48 +0930

    Added animation

--- src/main/java/com/mrcrayfish/dab/event/InputEvent.java
+++ src/main/java/com/mrcrayfish/dab/event/InputEvent.java
@@ -6,18 +6,24 @@ import com.mrcrayfish.dab.init.ModKeys;
 import com.mrcrayfish.dab.network.PacketHandler;
 import com.mrcrayfish.dab.network.message.MessageDab;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class InputEvent 
 {
 	public static boolean dabbing = false;
 	public static boolean printed = false;
+	
+	public static final int MAX_DABBING_HELD = 8;
+	public static int dabbingHeld = 0;
+	public static int prevDabbingHeld = 0;
+	
+	public static float firstPersonPartialTicks;
 	
 	@SubscribeEvent
 	public void onKeyInput(KeyInputEvent event)
@@ -38,6 +44,21 @@ public class InputEvent
 	}
 	
 	@SubscribeEvent
+	public void onTick(TickEvent.ClientTickEvent event) 
+	{
+		prevDabbingHeld = dabbingHeld;
+		
+		if(dabbing && dabbingHeld < MAX_DABBING_HELD) 
+		{
+			dabbingHeld++;
+		} 
+		else if(!dabbing && dabbingHeld > 0)
+		{
+			dabbingHeld--;
+		}
+	}
+	
+	@SubscribeEvent
 	public void onJoin(PlayerEvent.PlayerLoggedInEvent event)
 	{
 		if(!printed)
@@ -45,5 +66,11 @@ public class InputEvent
 			event.player.addChatMessage(new TextComponentString(TextFormatting.GOLD.toString() + TextFormatting.BOLD.toString() + "Press " + Keyboard.getKeyName(ModKeys.dab.getKeyCode()) + " to Dab!"));
 			printed = true;
 		}
+	}
+	
+	@SubscribeEvent
+	public void onRender(RenderHandEvent event) 
+	{
+		firstPersonPartialTicks = event.getPartialTicks();
 	}
 }
